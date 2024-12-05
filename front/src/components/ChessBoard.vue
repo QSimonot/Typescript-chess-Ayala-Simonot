a377838968015ef954b33c97fc56b129051d1da4
Sebastian <azerdumat@gmail.com>
2024-12-05 10:11:29 +0100

    front fixed

--- front/src/components/ChessBoard.vue
+++ front/src/components/ChessBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="board">
     <template v-for="(row, rowIndex) in board" :key="rowIndex">
-      <div v-for="(cell, colIndex) in row" :key="colIndex" :class="{ 'white': (rowIndex + colIndex) % 2 !== 0 }" @click="toggleColor" class="cell">
+      <div v-for="(cell, colIndex) in row" :key="colIndex" :class="{ 'white': (rowIndex + colIndex) % 2 !== 0 }" class="cell">
         {{ cell }}
       </div>
     </template>
@@ -9,10 +9,6 @@
 </template>
 
 <script setup lang="ts">
-
-function toggleColor(e){
-
-}
 
 const board =[
   ["R","N","B","Q","K","B","N","R"],
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,15 +1,1 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import AuthorList from '@/views/AuthorList.vue';
-import AuthenticationPage from '@/views/authentication/AuthenticationPage.vue';
-const routes = [
-  { path: '/', redirect: '/authors' },
-  { path: '/authors', component: AuthorList },
-  { path: '/connect', component: AuthenticationPage },
-];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-export default router;
