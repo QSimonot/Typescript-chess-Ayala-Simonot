849ad3bff1b112ed3c8092ffbe8351367da11b69
Sebastian <azerdumat@gmail.com>
2024-12-05 09:26:03 +0100

    backend + front

---
+++ front/src/components/ChessBoard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="board">
+    <template v-for="(row, rowIndex) in board" :key="rowIndex">
+      <div v-for="(cell, colIndex) in row" :key="colIndex" :class="{ 'white': (rowIndex + colIndex) % 2 !== 0 }" @click="toggleColor" class="cell">
+        {{ cell }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+function toggleColor(e){
+
+}
+
+const board =[
+  ["R","N","B","Q","K","B","N","R"],
+  ["P","P","P","P","P","P","P","P"],
+  ["-","-","-","-","-","-","-","-"],
+  ["-","-","-","-","-","-","-","-"],
+  ["-","-","-","-","-","-","-","-"],
+  ["-","-","-","-","-","-","-","-"],
+  ["P","P","P","P","P","P","P","P"],
+  ["R","N","B","Q","K","B","N","R"]
+];
+
+</script>
