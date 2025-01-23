4f832e0d05c1288b3817dc40f47da06f270b5cd0
QSimonot <122995270+QSimonot@users.noreply.github.com>
2025-01-23 09:36:13 +0100

    Delete build directory

--- build/backend/src/error/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-function notFound(name) {
-    const error = new Error(name + " not found");
-    error.status = 404;
-    throw error;
-}
--- build/front/src/composables/move/moveService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMove = getMove;
-const moveApi_1 = require("./moveApi");
-async function getMove(id) {
-    const getMyUser = await (0, moveApi_1.getMoveOfGame)(id);
-    return getMyUser;
-}
